b6938c6119e4aaad864f085ad3682d339bc71991
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2023-08-18 19:30:19 -0300

    Adiciona services (doc)

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,11 +3,20 @@ import { axiosInstance } from "@/services/axiosInstance";
 
 const localStorageKey = "todos";
 
+/**
+ * Salva as notas no localStorage
+ * @param data
+ * @returns Note[]
+ */
 const saveInDB = (data: any) => {
   localStorage.setItem(localStorageKey, JSON.stringify(data));
   return data;
 };
 
+/**
+ * Retorna todas as notas do localStorage
+ * @returns Note[]
+ */
 const getFromDB = () => {
   const dataStorage = localStorage.getItem(localStorageKey);
   if (!dataStorage) return [];
@@ -24,5 +33,23 @@ export const getNotes = async (): Promise<Note[]> => {
     return saveInDB(json);
   } catch {
     return getFromDB();
+  }
+};
+
+export const createNote = async (note: Note): Promise<void> => {
+  try {
+    await axiosInstance.post("/todo", note);
+  } catch {
+    saveInDB([...getFromDB(), note]);
+    throw new Error("Offline");
+  }
+};
+
+export const updateNote = async (note: Note): Promise<void> => {
+  try {
+    await axiosInstance.put("/todo", note);
+  } catch {
+    saveInDB([...getFromDB(), note]);
+    throw new Error("Offline");
   }
 };

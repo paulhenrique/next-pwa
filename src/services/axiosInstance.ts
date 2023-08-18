da0f1a00d97773a19ac7f6b1ecb6f10d25bad231
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2023-08-18 19:01:06 -0300

    Adiciona axiosInstance

---
+++ src/services/axiosInstance.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+/**
+ * Instância axios que cuida das requisições para a API
+ */
+export const axiosInstance = axios.create({
+  baseURL: "/api/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});

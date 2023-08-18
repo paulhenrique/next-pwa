32ae33c3793feeb2954a9706a04c0c84aa4c7c01
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2023-08-18 19:25:23 -0300

    Adiciona componente NoteBox

---
+++ src/components/NoteBox.tsx
@@ -0,0 +1,36 @@
+import { Paper, Typography, Box, Button } from "@mui/material";
+import React from "react";
+
+export interface NoteBoxProps {
+  notes: string;
+  onDelete?: () => void;
+}
+
+/**
+ * Componente de Notas
+ * @param param0
+ * @returns
+ */
+export const NoteBox: React.FC<NoteBoxProps> = ({ notes, onDelete }) => {
+  return (
+    <Paper
+      sx={{
+        padding: "1rem",
+      }}
+    >
+      <Typography variant="body1">{notes}</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          gap: "1rem",
+          marginTop: "1rem",
+          justifyContent: "flex-end",
+        }}
+      >
+        <Button onClick={onDelete}>Excluir</Button>
+      </Box>
+    </Paper>
+  );
+};
+
+export default NoteBox;

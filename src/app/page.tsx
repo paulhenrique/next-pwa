458f4380c3965dae2e7e01f82a3454e5c21230c9
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2023-08-18 19:46:12 -0300

    Refatora página de listagem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,160 +1,68 @@
 "use client";
 import React, { useState } from "react";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { withProviderClient } from "@/components/ProviderClient";
-import {
-  Box,
-  Button,
-  Container,
-  CssBaseline,
-  Paper,
-  TextField,
-  Typography,
-} from "@mui/material";
-import { Todo } from "@/pages/api/todo";
-import OnlineViewer, { useOnline } from "@/components/OnlineViewer";
-import axios from "axios";
-
-const localStorageKey = "todos";
-
-const getTodos = async (): Promise<Todo[]> => {
-  try {
-    console.log("loading online =========================");
-    const { data: json } = await axios.get("/api/todo");
-    localStorage.setItem(localStorageKey, JSON.stringify(json));
-    return json;
-  } catch {
-    console.log("loading offline =========================");
-    const dataStorage = localStorage.getItem(localStorageKey);
-    if (!dataStorage) return [];
-    return JSON.parse(dataStorage);
-  }
-};
+import { Box, Container, CssBaseline, Fab, Typography } from "@mui/material";
+import useNotes from "@/hooks/useNotes";
+import NoteBox from "@/components/NoteBox";
+import { Add } from "@mui/icons-material";
+import NewNoteModal from "@/components/NewNoteModal";
 
 const Home = () => {
-  const { data = [] } = useQuery<Todo[]>(["todos"], getTodos);
-  const [todo, setTodo] = useState<Todo>({} as Todo);
+  const {
+    getAll: { data: notes },
+  } = useNotes();
 
-  const { online } = useOnline();
-  const queryClient = useQueryClient();
-  const { mutate } = useMutation(
-    ["todos"],
-    () => {
-      return fetch("/api/todo", {
-        method: "POST",
-        body: JSON.stringify({
-          ...todo,
-        }),
-      })
-        .then((response) => response.json())
-        .then((json) => {
-          if (!online) throw new Error("Offline");
-          queryClient.refetchQueries(["todos"]);
-          return json;
-        })
-        .catch(() => {
-          const dataStorage = localStorage.getItem(localStorageKey);
-          if (!dataStorage) {
-            localStorage.setItem(localStorageKey, JSON.stringify([todo]));
-            throw new Error("Offline");
-          }
-          const data = JSON.parse(dataStorage);
-          localStorage.setItem(
-            localStorageKey,
-            JSON.stringify([...data, todo])
-          );
-          queryClient.refetchQueries(["todos"]);
-          throw new Error("Offline");
-        });
-    },
-    {}
-  );
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
 
-  const { mutate: mutateTodo } = useMutation({
-    mutationKey: ["todos"],
-    mutationFn: (todoItem) => {
-      return fetch("/api/todo", {
-        method: "PUT",
-        body: JSON.stringify(todoItem),
-      })
-        .then((response) => response.json())
-        .then((json) => {
-          if (!online) throw new Error("Offline");
-          queryClient.refetchQueries(["todos"]);
-          return json;
-        })
-        .catch(() => {
-          const dataStorage = localStorage.getItem(localStorageKey);
-          if (!dataStorage) {
-            localStorage.setItem(localStorageKey, JSON.stringify([todo]));
-            throw new Error("Offline");
-          }
-
-          const data = JSON.parse(dataStorage);
-          localStorage.setItem(
-            localStorageKey,
-            JSON.stringify([...data, todo])
-          );
-          queryClient.refetchQueries(["todos"]);
-          throw new Error("Offline");
-        });
-    },
-  });
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <Box
       sx={{
-        padding: "4rem",
+        padding: "1rem",
+        height: "100vh",
+        width: "100vw",
+        background: "#f2f1f1",
       }}
     >
       <CssBaseline />
-      <Container>
-        <OnlineViewer />
+      <Container
+        sx={{
+          maxWidth: {
+            md: "60%",
+            sx: "80%",
+            xs: "100%",
+          },
+        }}
+      >
         <Box display="flex" flexDirection="column" gap="1rem">
-          <TextField
-            onChange={({ target: { value } }) => {
-              setTodo((current) => ({
-                ...current,
-                title: value,
-              }));
-            }}
-            value={todo?.title}
-            label="Título"
-          />
-          <TextField
-            onChange={({ target: { value } }) => {
-              setTodo((current) => ({
-                ...current,
-                description: value,
-              }));
-            }}
-            value={todo?.description}
-            label="Descrição"
-          />
-          <Button onClick={() => mutate()}>Criar novo</Button>
-        </Box>
-
-        {!data?.length && <Typography variant="body1">No todos</Typography>}
-        <Box display="flex" flexDirection="column" gap="1rem">
-          {data?.map((todo) => (
-            <Paper sx={{ padding: "1rem" }} key={todo.id}>
-              <Typography variant="body1">{todo.title}</Typography>
-              <Typography variant="body2">{todo.description}</Typography>
-              <Typography variant="body2">
-                {todo.completed ? "Finalizado" : "Não finalizado"}
-              </Typography>
-              <Button
-                onClick={() =>
-                  mutateTodo({ ...todo, completed: !todo.completed } as any)
-                }
-                variant="contained"
-                color="primary"
-              >
-                Completar
-              </Button>
-            </Paper>
+          {notes?.length === 0 && (
+            <Typography color="textSecondary">
+              Sem notas encontradas, clique abaixo para criar uma nova nota
+            </Typography>
+          )}
+          {notes?.map((note) => (
+            <NoteBox key={note.id} notes={note.description} />
           ))}
         </Box>
+        <NewNoteModal handleClose={handleClose} open={open} />
+        <Fab
+          sx={{
+            position: "absolute",
+            bottom: 16,
+            right: 16,
+          }}
+          color="primary"
+          aria-label="add"
+          onClick={handleOpen}
+        >
+          <Add />
+        </Fab>
       </Container>
     </Box>
   );

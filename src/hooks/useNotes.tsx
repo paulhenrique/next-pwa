a6811fe13121470731e7262bb7a2e238e07f5291
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2023-08-18 19:14:08 -0300

    Adiciona documentação base

--- src/hooks/useNotes.tsx
+++ src/hooks/useNotes.tsx
@@ -2,11 +2,21 @@ import { Note } from "@/pages/api/todo";
 import { createNote, getNotes, updateNote } from "@/services";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
+/**
+ * Hook que retorna as funções de manipulação de notas
+ * @returns
+ */
 const useNotes = () => {
   const queryClient = useQueryClient();
 
+  /**
+   * Query para buscar todas as notas
+   */
   const getAll = useQuery<Note[]>(["notes"], getNotes);
 
+  /**
+   * Mutation para criar uma nota
+   */
   const create = useMutation(["notes"], async (element: Note) => {
     try {
       await createNote(element);
@@ -15,6 +25,9 @@ const useNotes = () => {
     }
   });
 
+  /**
+   * Mutation para atualizar uma nota
+   */
   const update = useMutation(["notes"], async (element: Note) => {
     try {
       await updateNote(element);
